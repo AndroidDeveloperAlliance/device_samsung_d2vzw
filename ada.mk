57a45ad2e2e429dc94a78b2c139d51a29e59cacc
Blueharford <blueharford@gmail.com>
2012-09-18 18:00:37 -0500

    fix build
    Change-Id: Iaa8658b116695ea264d0bf2a465309c6f3b7a5d2

--- ada.mk
+++ ada.mk
@@ -9,8 +9,20 @@ $(call inherit-product, vendor/ada/config/nfc_enhanced.mk)
 # Inherit some common ADA stuff.
 $(call inherit-product, vendor/ada/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FINGERPRINT="d2vzw-user 4.0.4 IMM76D I535VRALG7 release-keys" PRIVATE_BUILD_DESC="Verizon/d2vzw/d2vzw:4.0.4/IMM76D/I535VRALG7:user/release-keys"
-
-PRODUCT_NAME := ada_d2vzw
+# products
 PRODUCT_DEVICE := d2vzw
+PRODUCT_BRAND := Verizon
+PRODUCT_NAME := ada_d2vzw
+PRODUCT_MODEL := SCH-I535
+PRODUCT_MANUFACTURER := Samsung
+
+# overrides
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_NUMBER=I535VRALG7 \
+    PRODUCT_NAME=d2vzw \
+    TARGET_DEVICE=d2vzw \
+    TARGET_BUILD_TYPE=user \
+    BUILD_VERSION_TAGS=release-keys \
+    PRIVATE_BUILD_DESC="d2vzw-user 4.0.4 IMM76D I535VRALG7 release-keys" \
+    BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.0.4/IMM76D/I535VRALG7:user/release-keys"
 
